31113be95b5f0c9ff3f0d70feb5ebfc588afe505
허승경 <hhe98hsk@gmail.com>
2025-01-04 15:21:03 +0900

    feat: Runnable 인터페이스

--- practice0104/src/study0104/Test02.java
+++ practice0104/src/study0104/Test02.java
@@ -4,7 +4,29 @@ public class Test02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Thread t1 = new Thread(new Movie());		// 1. new Thread()로 스레드 객체 생성
+		Thread t2 = new Thread(() ->  {
+			for(int i = 1; i <= 1000; i++) {
+				System.out.println("전화 받기 : " + i);
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		for(int i = 1; i <= 1000; i++) {
+			System.out.println("TV 보기 : " + i);
+		}
 	}
+}
 
+/* Movie는 Runnable 인터페이스 상속 받음
+ * run() 메소드를 오버라이딩해서 스레드에서 실행할 내용 구현
+ */
+class Movie implements Runnable{
+	@Override
+	public void run() {
+		for(int i = 1; i <= 1000; i++) {
+			System.out.println("음식 먹기 : " + i);
+		}
+	}
 }
