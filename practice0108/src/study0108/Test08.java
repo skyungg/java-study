8f1d4421afa1c3c10ce51c5ca92a97edc9614566
허승경 <hhe98hsk@gmail.com>
2025-01-08 13:29:57 +0900

    feat: sleep 메서드

---
+++ practice0108/src/study0108/Test08.java
@@ -0,0 +1,23 @@
+package study0108;
+
+public class Test08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintThread t = new PrintThread();
+		t.start();
+	}
+}
+
+class PrintThread extends Thread{
+	public void run() {
+		try {
+			for(int i = 0; i < 10; i++) {
+				Thread.sleep(2000);
+				System.out.println("출력 : 0"+ i);
+			}
+		}catch(InterruptedException e) {
+			System.out.println("Interrupt가 발생하여 Sleep 상태를 종료함");
+		}
+	}
+}
